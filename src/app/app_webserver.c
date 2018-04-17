d1e2c557f5349c1c5e8244f448de741bfff909e4
Hyouck Hawk Kim <hawk.kim1974@gmail.com>
2018-04-17 11:15:03 +0900

    adding scaffolding for web service api

--- src/app/app_webserver.c
+++ src/app/app_webserver.c
@@ -13,6 +13,37 @@
 static pthread_t    _mongoose_thread;
 static struct mg_serve_http_opts s_http_server_opts;
 
+static struct mg_str    _api_v1_root = MG_MK_STR("/api/v1/");
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// utilities
+//
+///////////////////////////////////////////////////////////////////////////////
+static inline const char*
+c_str_from_mg_str(struct mg_str ms)
+{
+  static char str[1024 + 1];
+  int   len;
+
+  len = ms.len <= 1024 ? ms.len : 1024;
+  strncpy(str, ms.p, len);
+  str[len] = '\0';
+
+  return str;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// JSON REST API Handler
+//
+///////////////////////////////////////////////////////////////////////////////
+static void
+__handle_api_v1_request(struct mg_connection* nc, struct http_message* hm)
+{
+  // FIXME
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 //
 // mongoose event handler
@@ -26,12 +57,13 @@ ev_handler(struct mg_connection* nc, int ev, void* ev_data)
   switch(ev)
   {
   case MG_EV_HTTP_REQUEST:
-    if(mg_vcmp(&hm->uri, "/api/v1/sum") == 0)
+    if(mg_strncmp(hm->uri, _api_v1_root, _api_v1_root.len) == 0)
     {
-      // FIXME
+      TRACE(APP_WEB, "api request: %s\n", c_str_from_mg_str(hm->uri));
+      __handle_api_v1_request(nc, hm);
     }
     else if(mg_vcmp(&hm->uri, "/printcontent") == 0) 
-    {
+    { // just for a test
       char buf[100] = {0};
 
       memcpy(buf, hm->body.p, sizeof(buf) - 1 < hm->body.len ? sizeof(buf) - 1 : hm->body.len);
@@ -60,7 +92,7 @@ __mongoose_thread(void* arg)
   struct mg_mgr           mgr;
   struct mg_connection*   nc;
 
-  TRACE(APP_WEB, "starting web server\n");
+  TRACE(APP_WEB, "starting web server for port %s\n", WEB_SERVER_PORT);
   mg_mgr_init(&mgr, NULL);
 
   nc = mg_bind(&mgr, WEB_SERVER_PORT, ev_handler);
