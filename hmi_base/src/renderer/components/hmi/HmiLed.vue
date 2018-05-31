709a7e9c77c12e26d6a0a627bba3e2d6cf187a93
Hyouck Hawk Kim <hawk.kim1974@gmail.com>
2018-05-31 14:30:23 +0900

    added HMI LED

---
+++ hmi_base/src/renderer/components/hmi/HmiLed.vue
@@ -0,0 +1,197 @@
+<template>
+  <svg viewBox="0 0 6 6" :width="width" :height="height">
+    <defs>
+      <radialGradient
+          id="onGreen1"
+          gradientUnits="userSpaceOnUse"
+          cy="21.5"
+          cx="47.5"
+          r="2.5"
+        >
+        <stop style="stop-color:#00c300;stop-opacity:0" offset="0" />
+        <stop style="stop-color:#00c300;stop-opacity:.49804" offset="0.82" />
+        <stop style="stop-color:#aeffae" offset="1"
+        />
+      </radialGradient
+      >
+      <radialGradient
+          id="onGreen2"
+          gradientUnits="userSpaceOnUse"
+          cy="21.5"
+          cx="47.5"
+          r="2.5"
+        >
+        <stop style="stop-color:#ffffff" offset="0" />
+        <stop style="stop-color:#0dee1b;stop-opacity:0" offset="1" />
+      </radialGradient
+      >
+      <radialGradient
+          id="borderStyle"
+          cx="-87.5"
+          gradientUnits="userSpaceOnUse"
+          cy="62.5"
+          r="2.5"
+        >
+        <stop style="stop-color:#000000" offset="0" />
+        <stop style="stop-color:#000000" offset=".51649" />
+        <stop style="stop-color:#a6a6a6" offset="0.76" />
+        <stop style="stop-color:#353535" offset="0.9" />
+        <stop style="stop-color:#000000" offset="1" />
+      </radialGradient>
+
+      <linearGradient id="offGreen1" gradientUnits="userSpaceOnUse">
+        <stop style="stop-color:#006400" />
+        <stop style="stop-color:#006400" />
+      </linearGradient>
+
+      <linearGradient id="offGreen2">
+      </linearGradient>
+
+      <!-- red color -->
+      <radialGradient
+        id="onRed1"
+        gradientUnits="userSpaceOnUse"
+        cy="21.5"
+        cx="47.5"
+        r="2.5"
+      >
+        <stop style="stop-color:#fc0000;stop-opacity:0.7" offset="0" />
+        <stop style="stop-color:#fc0000;stop-opacity:.99804" offset="0.82" />
+        <stop style="stop-color:#ffaeae" offset="1" />
+      </radialGradient
+      >
+      <radialGradient
+        id="onRed2"
+        gradientUnits="userSpaceOnUse"
+        cy="21.5"
+        cx="47.5"
+        r="2.5"
+      >
+        <stop style="stop-color:#fff0f0" offset="0" />
+        <stop style="stop-color:#ee0d0d;stop-opacity:0" offset="1" />
+      </radialGradient
+      >
+      <linearGradient id="offRed1" gradientUnits="userSpaceOnUse">
+        <stop style="stop-color:#6B0000" />
+        <stop style="stop-color:#6B0000" />
+      </linearGradient>
+      <linearGradient id="offRed2">
+      </linearGradient>
+
+      <!-- blue color -->
+      <radialGradient id="onBlue1" gradientUnits="userSpaceOnUse"
+        cy="21.5"
+        cx="47.5"
+        r="2.5">
+        <stop style="stop-color:#005dc3;stop-opacity:0.7" offset="0"/>
+        <stop style="stop-color:#0066c3;stop-opacity:.99804" offset="0.82"/>
+        <stop style="stop-color:#aec5ff" offset="1"/>
+      </radialGradient>
+
+      <radialGradient id="onBlue2" gradientUnits="userSpaceOnUse"
+        cy="21.5"
+        cx="47.5"
+        r="2.5">
+        <stop style="stop-color:#ffffff" offset="0"/>
+        <stop style="stop-color:#0d78ee;stop-opacity:0" offset="1"/>
+      </radialGradient>
+
+      <linearGradient id="offBlue1" gradientUnits="userSpaceOnUse">
+        <stop style="stop-color:#00006B" />
+        <stop style="stop-color:#00006B" />
+      </linearGradient>
+      <linearGradient id="offBlue2">
+      </linearGradient>
+
+    </defs>
+    <sodipodi:namedview
+        bordercolor="#666666"
+        pagecolor="#ffffff"
+        showgrid="false"
+        borderopacity="1.0"
+    />
+    <g
+        id="layer1"
+        transform="translate(-437 -640.79)"
+      >
+      <path
+          id="path6189-3"
+          sodipodi:rx="2.5"
+          sodipodi:ry="2.5"
+          v-bind:style="style3"
+          sodipodi:type="arc"
+          d="m-85 62.5c0 1.3807-1.1193 2.5-2.5 2.5s-2.5-1.1193-2.5-2.5 1.1193-2.5 2.5-2.5 2.5 1.1193 2.5 2.5z"
+          transform="matrix(1.2 0 0 1.2 545 568.79)"
+          sodipodi:cy="62.5"
+          sodipodi:cx="-87.5"
+      />
+      <path
+          id="path6983"
+          sodipodi:rx="2.5"
+          sodipodi:ry="2.5"
+          style="fill:#00c300"
+          sodipodi:type="arc"
+          d="m50 21.5c0 1.381-1.119 2.5-2.5 2.5s-2.5-1.119-2.5-2.5 1.119-2.5 2.5-2.5c1.3807 0 2.5 1.1193 2.5 2.5z"
+          transform="translate(392.5 622.29)"
+          sodipodi:cy="21.5"
+          sodipodi:cx="47.5"
+      />
+      <path
+          id="path6985"
+          sodipodi:rx="2.5"
+          sodipodi:ry="2.5"
+          v-bind:style="style2"
+          sodipodi:type="arc"
+          d="m50 21.5c0 1.381-1.119 2.5-2.5 2.5s-2.5-1.119-2.5-2.5 1.119-2.5 2.5-2.5c1.3807 0 2.5 1.1193 2.5 2.5z"
+          transform="translate(392.9 621.89)"
+          sodipodi:cy="21.5"
+          sodipodi:cx="47.5"
+      />
+      <path
+          id="path6995"
+          sodipodi:rx="2.5"
+          sodipodi:ry="2.5"
+          v-bind:style="style1"
+          sodipodi:type="arc"
+          d="m50 21.5c0 1.381-1.119 2.5-2.5 2.5s-2.5-1.119-2.5-2.5 1.119-2.5 2.5-2.5c1.3807 0 2.5 1.1193 2.5 2.5z"
+          transform="translate(392.5 622.29)"
+          sodipodi:cy="21.5"
+          sodipodi:cx="47.5"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'HmiLed',
+    computed: {
+      style1 () {
+        if (this.ledOn) {
+          return 'fill:url(#on' + this.color + '1)'
+        } else {
+          return 'fill:url(#off' + this.color + '1)'
+        }
+      },
+      style2 () {
+        if (this.ledOn) {
+          return 'fill:url(#on' + this.color + '2)'
+        } else {
+          return 'fill:url(#off' + this.color + '2)'
+        }
+      },
+      style3 () {
+        return 'fill:url(#borderStyle)'
+      }
+    },
+    props: {
+      width: { type: Number },
+      height: { type: Number },
+      color: { type: String },
+      ledOn: { type: Boolean, default: false }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
