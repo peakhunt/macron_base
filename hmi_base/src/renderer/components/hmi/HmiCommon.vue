dd61daf7a4145d6749ef704de1d70efb17d229fd
Hyouck Hawk Kim <hawk.kim1974@gmail.com>
2018-05-31 14:30:58 +0900

    added HmiCommon for better code organization

---
+++ hmi_base/src/renderer/components/hmi/HmiCommon.vue
@@ -0,0 +1,23 @@
+<script>
+  import {EventBus} from '@/event-bus'
+
+  export default {
+    name: 'HmiCommon',
+    created () {
+      EventBus.$on('global500msTick', this.toggleTick)
+    },
+    beforeDestroy () {
+      EventBus.$off('global500msTick', this.toggleTick)
+    },
+    methods: {
+      toggleTick () {
+        this.tickValue = !this.tickValue
+      }
+    },
+    data () {
+      return {
+        tickValue: false
+      }
+    }
+  }
+</script>
